3de9959e59f3817ccfb925f6d0f43ebebf53ec85
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-30 18:30:06 +0300

    finishes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = ''.join(sorted(word))
+
+    def match(self, words_list):
+        return [word for word in words_list if self._is_it_anagram(word)]
+
+    def _is_it_anagram(self, word):
+        return ''.join(sorted(word)) == self.sorted_word
+    pass
